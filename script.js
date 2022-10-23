e530d223cd7e4498ca3826eb92a6cd103ee6d3a7
Pedro Elias <106087744+Elias-Pdr@users.noreply.github.com>
2022-10-23 01:29:45 -0300

    up 21

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 function btnComprar(){
-    window.alert("Só funcina até ai msm")
+    window.location.href = "telaPagamento.html"
+}
+
+
+
+document.getElementById("tel").addEventListener("keypress", function(e){
+    telValueLength = document.getElementById("tel").value.length;
+    console.log(telValueLength);
+    if (telValueLength === 2) {
+        document.getElementById("tel").value = "(" + document.getElementById("tel").value + ") "
+    }
+})
+
+
+function mudarCorBtnCompra(bnt){
+    var listIconPay = document.querySelectorAll(".iconPay");
+    var listContainerPay = document.querySelectorAll(".containerPay");
+    
+    console.log(listIconPay.length);
+
+    for (let i = 0; i < listIconPay.length; i++) {
+        listIconPay[i].style.fill = "black";
+        listContainerPay[i].style.display = "none";
+    }
+
+    document.getElementById(bnt).style.fill = "var(--colorSecundary)";
+    document.getElementById("pay" + bnt).style.display = "block";
 }
